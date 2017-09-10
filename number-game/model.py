d114da3f4a7770d83f1103855ec1dac9f0f6a691
yzh119 <expye@outlook.com>
2017-09-10 15:26:35 +0000

    update model

--- number-game/model.py
+++ number-game/model.py
@@ -3,23 +3,66 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def Receiver(nn.Module):
-    def __init__(self):
-        pass
+"""
+Format:
+input vector: (2n + 1)
+output vector(prob): (2)
+"""
+class Receiver(nn.Module):
+    def __init__(self, n, hid):
+        super(Receiver, self).__init__()
+        self.n = n
+        self.hid = hid
+        self.net = nn.Sequential(
+            nn.Linear(2 * n + 1, hid),
+            nn.SELU(),
+            nn.Linear(hid, 2),
+            nn.Softmax()
+        )
     
-    def forward(self):
-        pass
+    def forward(self, input):
+        input_size = input.size(1)
+        assert input_size == self.n * 2 + 1
+        return self.net(input)
 
-def SenderActor(nn.Module):
-    def __init__(self):
-        pass
+"""
+Format:
+input vector: (2n)
+output vector(real number range from 0 to 1): (1)
+"""
+class SenderActor(nn.Module):
+    def __init__(self, n, hid):
+        super(SenderActor, self).__init__()
+        self.n = n
+        self.hid = hid
+        self.net = nn.Sequential(
+            nn.Linear(2 * n, hid),
+            nn.SELU(),
+            nn.Linear(hid, 1)
+        )
     
-    def forward(self):
-        pass
+    def forward(self, input):
+        input_size = input.size(1)
+        assert input_size == self.n * 2
+        return self.net(input)
 
-def SenderCritic(nn.Module):
-    def __init__(self):
-        pass
+"""
+Format:
+input vector: (2n + 1)
+output vector(Q value): (1)
+"""
+class SenderCritic(nn.Module):
+    def __init__(self, n, hid):
+        super(SenderCritic, self).__init__()
+        self.n = n
+        self.hid = hid
+        self.net = nn.Sequential(
+            nn.Linear(2 * n, hid),
+            nn.SELU(),
+            nn.Linear(hid, 1)
+        )
     
-    def forward(self):
-        pass
+    def forward(self, input):
+        input_size = input.size(1)
+        assert input_size == self.n * 2 + 1
+        return self.net(input)
--- number-game/train.py
+++ number-game/train.py
@@ -1,9 +1,39 @@
 
-"""
-About Deterministic Policy Gradient:
-"""
-
 from environment import Game
+from model import *
+from itertools import count
+import torch.optim as optim
+import logging
 
 bound = [0., 1.]
+
+# Hyper parameters
 explore_sigma = 0.05
+n_r = 10 
+
+n_numbers = 10
+n_hidden = 50
+n_games = 32
+game_pool = []
+for i in range(n_games):
+    game_pool.append(Game(n_numbers))
+
+R = Receiver(n_numbers, n_hidden)
+SA = SenderActor(n_numbers, n_hidden)
+SC = SenderCritic(n_numbers, n_hidden)
+
+optim_r = optim.SGD(R.parameters(), lr=1e-4)
+optim_sa = optim.SGD(SA.parameters(), lr=1e-4)
+optim_sc = optim.SGD(SC.parameters(), lr=1e-4)
+
+running_succ_rate = 0
+for epoch in count():
+    succ_rate = 0
+    for i in range(n_games):
+        game_pool[i].reset()
+    
+    
+    
+    
+    running_succ_rate = running_succ_rate * 0.95 + succ_rate * 0.05
+    print('successful_rate = {}'.format(running_succ_rate))
